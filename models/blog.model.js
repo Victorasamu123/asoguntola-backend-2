15f5e96ac22e2dc738d075a6b0e57bf44b59aa3c
Avanin <asamuvictor03@gmail.com>
2023-05-17 23:11:33 +0100

    made an update in blog schema

--- models/blog.model.js
+++ models/blog.model.js
@@ -2,13 +2,7 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema;
 
-const blogSchema = new Schema({
-    productimage:{type:String, required:false},
-    title:{type:String, required:false},
-    decription:{type:String, required:false},
-    selectedValue:{type:String, required:false},
-    currentDate:{type:Object, required:false}
-})
+const blogSchema = new Schema({}, { strict: false })
 
 const blogModel = mongoose.model("asoguntola-blog-post_collections",blogSchema);
 
