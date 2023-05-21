17b0600df96b3d909b4301ed6dd2322dbe2d3351
Avanin <asamuvictor03@gmail.com>
2023-05-21 01:07:34 +0100

    updated blog upload controller

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 app.use(cors())
 app.use(express.urlencoded({limit: "200mb",extended:true}));
-app.use(express.json())
-app. use(express. json({limit: '50mb'}));
+app. use(express. json({limit: '200mb'}));
 const PORT = process.env.PORT || 5000
 const URI  = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
